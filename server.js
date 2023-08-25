ddc8c8abbed8fd05592d467cfaa350a3a261aead
Mohd-Iqbal <121681856+Mohd-Iqbal@users.noreply.github.com>
2023-08-25 09:03:24 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const app = express()
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://64e8b79008d2d8259c6633ba--starlit-puffpuff-146583.netlify.app');
+    res.header('Access-Control-Allow-Credentials', 'true')
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     
