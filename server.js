1577a156232bc358f8f95ce2b1bb865d5f160528
Mohd-Iqbal <121681856+Mohd-Iqbal@users.noreply.github.com>
2023-08-25 23:14:33 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from "express"
 import morgan from "morgan"
 import cookieParser from "cookie-parser"
-// import cors from "cors"
+import path from "path"
+import { fileURLToPath } from 'url';
 import mongoose from "mongoose"
 import users from "./Routes/signUpRoutes.js"
 import login from "./Routes/loginRoute.js"
@@ -14,43 +15,34 @@ dotenv.config()
 
 const app = express()
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://64e8b79008d2d8259c6633ba--starlit-puffpuff-146583.netlify.app');
-    res.header('Access-Control-Allow-Credentials', 'true')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    
-    if (req.method === 'OPTIONS') {
-      res.sendStatus(200);
-    } else {
-      next();
-    }
-  });
   
 app.use(cookieParser())
 app.use(morgan("tiny"))
-// app.use(cors({
-//     origin: process.env.FRONTEND_URL,
-//     credentials:true, 
-//     methods: 'GET,POST,PUT,DELETE,OPTIONS',
-//     allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
-// }))
 app.use(express.json())
 
-app.get("/",(req,res) => {
-    res.send("hello from the server")
-})
 
 app.use("/api/signup",users)
 app.use("/api/login",login)
 app.use("/api/profile",profile)
 app.use("/api/todo",todo)
 
-mongoose.set('strictQuery', false);
-mongoose.connect("mongodb+srv://iqbal:123ithinkso123@cluster0.vzl69kn.mongodb.net/")
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "/dist")))
+
+app.get("*",(req,res) => {
+  const file = path.join(__dirname, "/dist/html.index")
+  if(file) {
+    res.sendFile(file)
+  } else {
+    res.status(500).send("something wrong happen")
+  }
+})
+
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb+srv://iqbal:123ithinkso123@cluster0.vzl69kn.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>{console.log(`Connected to mongodb...`)})
-// mongodb+srv://iqbal:123ithinkso123@cluster0.vzl69kn.mongodb.net/?retryWrites=true&w=majority
 
 app.listen(process.env.PORT,()=>{
-    console.log("Server connected to http://localhost:8080")
+    console.log("Server connected...")
 })
